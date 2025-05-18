b5e12e018e965f07f1410605388b2a42589133e9
Leandro <leandroalegregabriel@outlook.com>
2025-05-18 12:12:50 -0300

    feat(view): agrega principal.php como vista principal

--- app/Views/front/principal.php
+++ app/Views/front/principal.php
@@ -1,54 +1,62 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Pagina de Prueba</title>
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-</head>
-
 <body>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+  <section class="px-0">
+    <div id="mainCarousel" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="assets/img/banner/banner-1.jpg" class="d-block w-100" alt="">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Bienvenidos</h5>
+            <p>Curiosidades sobre los perros</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="assets/img/banner/banner-2.jpg" class="d-block w-100" alt="">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>¿Sabías que?</h5>
+            <p>Los perros pueden oler enfermedades.</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#mainCarousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+      <button class="carousel-control-next" type="button" data-bs-target="#mainCarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </button>
+    </div>
+  </section>
+
+  <section class="container mt-5">
+    <div class="row">
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <img src="assets/img/perfil/1-perfil.jpg" class="card-img-top" alt="María López">
+          <div class="card-body">
+            <h5 class="card-title">María López</h5>
+            <p class="card-text">Fundadora y directora. Amante de los perros.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <img src="assets/img/perfil/3-perfil.jpg" class="card-img-top" alt="Juan Pérez">
+          <div class="card-body">
+            <h5 class="card-title">Juan Pérez</h5>
+            <p class="card-text">Veterinario especializado. Encargado de salud animal.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <img src="assets/img/perfil/2-perfil.jpg" class="card-img-top" alt="Lucía Torres">
+          <div class="card-body">
+            <h5 class="card-title">Lucía Torres</h5>
+            <p class="card-text">Responsable de adopciones y bienestar canino.</p>
+          </div>
+        </div>
       </div>
     </div>
-  </nav>
-  <h1>Probando bootstrap</h1>
-  <script src="assets/js/bootstrap.bundle.min.js"></script>
-</body>
+  </section>
 
-</html>
+
+</body>
