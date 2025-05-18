21e57dcc0d342a376536666d406edaecbd893f71
Leandro <leandroalegregabriel@outlook.com>
2025-05-18 12:11:26 -0300

    feat(view): agrega navbar_view con estructura de navegación

---
+++ app/Views/front/navbar_view.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="logo.png" alt="Logo" height="30"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item"><a class="nav-link" href="/">Inicio</a></li>
+        <li class="nav-item"><a class="nav-link" href="/quienes-somos">Quiénes Somos</a></li>
+        <li class="nav-item"><a class="nav-link" href="/acerca">Acerca de</a></li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Usuarios</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/registrarse">Registrarse</a></li>
+            <li><a class="dropdown-item" href="/login">Login</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Buscar...">
+        <button class="btn btn-outline-light" type="submit">Buscar</button>
+      </form>
+    </div>
+  </div>
+</nav>
