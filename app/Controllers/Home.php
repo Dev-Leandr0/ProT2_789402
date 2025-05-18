c52e58e1d5350989f7683a154ca9d940b620056d
Leandro <leandroalegregabriel@outlook.com>
2025-05-18 12:25:24 -0300

    fix(controller): actualiza Home.php para cargar vistas head, navbar, principal y footer

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('front/principal');
+        return view('front/head_view') .  view('front/navbar_view') . view('front/principal') . view('front/footer_view');
     }
 }
