ba953c59080fd96388201543da38726fd39aac17
Leandro <leandroalegregabriel@outlook.com>
2025-05-19 09:46:59 -0300

    feat(view): Se agrega sección banner con carrusel debajo del navbar en principal.php

--- app/Views/front/principal.php
+++ app/Views/front/principal.php
@@ -1,62 +1,36 @@
-<body>
-  <section class="px-0">
-    <div id="mainCarousel" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="assets/img/banner/banner-1.jpg" class="d-block w-100" alt="">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Bienvenidos</h5>
-            <p>Curiosidades sobre los perros</p>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="assets/img/banner/banner-2.jpg" class="d-block w-100" alt="">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>¿Sabías que?</h5>
-            <p>Los perros pueden oler enfermedades.</p>
-          </div>
+<section class="banner-carrusel">
+  <div id="banner-carrusel-principal" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="assets/img/banner/banner-1.jpg" class="d-block w-100" alt="Bienvenidos Oracle Red Bull Racing">
+        <div class="carousel-caption banner-contenido">
+          <h2>Bienvenidos</h2>
+          <p>Oracle Red Bull Racing - Potencia y pasión en cada vuelta</p>
+          <a href="#explorar" class="btn btn-danger btn-lg">Explorar temporada</a>
         </div>
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#mainCarousel" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon"></span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#mainCarousel" data-bs-slide="next">
-        <span class="carousel-control-next-icon"></span>
-      </button>
-    </div>
-  </section>
-
-  <section class="container mt-5">
-    <div class="row">
-      <div class="col-md-4 mb-4">
-        <div class="card">
-          <img src="assets/img/perfil/1-perfil.jpg" class="card-img-top" alt="María López">
-          <div class="card-body">
-            <h5 class="card-title">María López</h5>
-            <p class="card-text">Fundadora y directora. Amante de los perros.</p>
-          </div>
+      <div class="carousel-item">
+        <img src="assets/img/banner/banner-2.jpg" class="d-block w-100" alt="Curiosidades Red Bull Racing">
+        <div class="carousel-caption banner-contenido">
+          <h2>¿Sabías que?</h2>
+          <p>Red Bull Racing es pionero en innovación tecnológica en la Fórmula 1</p>
+          <a href="#" class="btn btn-danger btn-lg">Más información</a>
         </div>
       </div>
-      <div class="col-md-4 mb-4">
-        <div class="card">
-          <img src="assets/img/perfil/3-perfil.jpg" class="card-img-top" alt="Juan Pérez">
-          <div class="card-body">
-            <h5 class="card-title">Juan Pérez</h5>
-            <p class="card-text">Veterinario especializado. Encargado de salud animal.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-4">
-        <div class="card">
-          <img src="assets/img/perfil/2-perfil.jpg" class="card-img-top" alt="Lucía Torres">
-          <div class="card-body">
-            <h5 class="card-title">Lucía Torres</h5>
-            <p class="card-text">Responsable de adopciones y bienestar canino.</p>
-          </div>
+      <div class="carousel-item">
+        <img src="assets/img/banner/banner-3.jpg" class="d-block w-100" alt="Curiosidades Red Bull Racing">
+        <div class="carousel-caption banner-contenido">
+          <h2>¿Sabías que?</h2>
+          <p>Red Bull Racing es pionero en innovación tecnológica en la Fórmula 1</p>
+          <a href="#" class="btn btn-danger btn-lg">Más información</a>
         </div>
       </div>
     </div>
-  </section>
-
-
-</body>
+    <button class="carousel-control-prev" type="button" data-bs-target="#banner-carrusel-principal" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#banner-carrusel-principal" data-bs-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </button>
+  </div>
+</section>
