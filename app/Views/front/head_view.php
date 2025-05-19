bb480ee07bf65d4932b5354bb2836633df3992bb
Leandro <leandroalegregabriel@outlook.com>
2025-05-19 09:45:24 -0300

    feat(view): Se agrega style.css y librería de íconos Bootstrap en head_view

--- app/Views/front/head_view.php
+++ app/Views/front/head_view.php
@@ -6,4 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Pagina de Prueba</title>
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/style.css">
 </head>
