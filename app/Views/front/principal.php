f0e8d6723eff6225422721af242299d8e96bf6a7
Leandro <leandroalegregabriel@outlook.com>
2025-05-19 17:55:17 -0300

    feat(view): agrega sección de cards de equipos en principal.php

--- app/Views/front/principal.php
+++ app/Views/front/principal.php
@@ -26,3 +26,44 @@
     </button>
   </div>
 </section>
+
+<section class="seccion-equipo container mt-5">
+  <h2 class="titulo-equipo mb-4 border-bottom border-danger pb-2">
+    Nuestro Equipo
+  </h2>
+
+  <div class="row">
+
+    <!-- Christian Horner -->
+    <div class="col-md-4 mb-4">
+      <div class="card bg-dark text-white border-0 shadow-lg">
+        <img src="assets/img/perfil/Christian-Horner-3.jpg" class="card-img-top" alt="Christian Horner">
+        <div class="card-body">
+          <h5 class="card-title text-primary">Christian Horner</h5>
+          <p class="card-text">Director de equipo. Estratega dentro y fuera de la pista.</p>
+        </div>
+      </div>
+    </div>
+    <!-- Pierre Waché -->
+    <div class="col-md-4 mb-4">
+      <div class="card bg-dark text-white border-0 shadow-lg">
+        <img src="assets/img/perfil/Pierre-Waché-2.jpg" class="card-img-top" alt="Pierre Waché">
+        <div class="card-body">
+          <h5 class="card-title text-warning">Pierre Waché</h5>
+          <p class="card-text">Diseñador jefe y mente maestra detrás del monoplaza de Red Bull.</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Helmut Marko -->
+    <div class="col-md-4 mb-4">
+      <div class="card bg-dark text-white border-0 shadow-lg">
+        <img src="assets/img/perfil/Hannah-Schmitz-1.jpg" class="card-img-top" alt="Helmut Marko">
+        <div class="card-body">
+          <h5 class="card-title text-danger"> Hannah Schmitz</h5>
+          <p class="card-text">Ingeniera jefe de estrategia en Red Bull Racing.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
