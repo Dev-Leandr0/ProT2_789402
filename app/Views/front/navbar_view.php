561055c3274d5a8756f0595c90b494db3ad57089
Leandro <leandroalegregabriel@outlook.com>
2025-05-20 08:58:11 -0300

    feat(view): agrega soporte de animaciones con Animate.css en el navbar

--- app/Views/front/navbar_view.php
+++ app/Views/front/navbar_view.php
@@ -1,8 +1,7 @@
-<nav class="navbar navbar-expand-lg sticky-top navbar-customizado">
-  <div class="container-fluid">
-    <a class="navbar-brand d-flex align-items-center" href="<?= site_url('principal') ?>">
-      <img src="<?= base_url('assets/img/logo/logo.png') ?>" alt="Red Bull Racing" height="40" class="me-2">
-      <span class="fw-bold text-uppercase text-warning">Red Bull Racing</span>
+<nav class="navbar navbar-expand-lg sticky-top navbar-customizado ">
+  <div class="container-fluid animate__animated animate__bounceInLeft">
+    <a class="navbar-brand d-flex align-items-center animate__animated animate__pulse animate__delay-1s animate__infinite" href="<?= site_url('principal') ?>">
+      <img src="<?= base_url('assets/img/logo/logo-2.svg') ?>" alt="Red Bull Racing" height="40" class="me-2">
     </a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -15,10 +14,10 @@
           <a class="nav-link active text-white fw-semibold" href="<?= site_url('principal') ?>">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white fw-semibold" href="/quienes-somos">Quiénes Somos</a>
+          <a class="nav-link text-white fw-semibold" href="/quienes-somos">Noticias</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white fw-semibold" href="/acerca">Acerca de</a>
+          <a class="nav-link text-white fw-semibold" href="/acerca">Monoplaza</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle text-white fw-semibold" href="#" role="button" data-bs-toggle="dropdown">
