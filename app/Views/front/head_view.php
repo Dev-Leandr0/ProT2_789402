8a36e586983cdfd6ddea9b39a2cd13c62ccc0549
Leandro <leandroalegregabriel@outlook.com>
2025-05-20 08:58:28 -0300

    feat(view): agrega favicon y enlace a Animate.css en head_view

--- app/Views/front/head_view.php
+++ app/Views/front/head_view.php
@@ -4,8 +4,10 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Pagina de Prueba</title>
+  <title>Red Bull Racing</title>
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet"> <!-- iconos bootstrap -->
   <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" /> <!-- animacion css -->
+  <link rel="shortcut icon" href="assets/img/logo/logo-3.png" type="image/x-icon">
 </head>
