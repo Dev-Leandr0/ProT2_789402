acbb33748e2b85fedbe3a6af56e453f7c46ab19d
Leandro <leandroalegregabriel@outlook.com>
2025-05-20 08:57:53 -0300

    refactor(view): reemplaza footer con Bootstrap por diseño con CSS Grid

--- app/Views/front/footer_view.php
+++ app/Views/front/footer_view.php
@@ -1,36 +1,38 @@
-<footer class="text-white pt-5 pb-4" style="background: linear-gradient(135deg, #000428, #004e92);">
-  <div class="container">
-    <div class="row">
+<footer class="footer">
+  <div class="footer-container">
+    <div class="footer-logo">
+      <a href="/" class="footer-marca">
+        <img src="assets/img/logo/logo-4.jpg" alt="Red Bull Racing" height="40">
+        <span>Red Bull Racing</span>
+      </a>
+      <p class="footer-description">
+        Velocidad, innovación y pasión por la Fórmula 1. Vive la emoción con nosotros.
+      </p>
+    </div>
 
-      <div class="col-md-4 mb-4">
-        <a href="/" class="d-flex align-items-center mb-3">
-          <img src="assets/img/logo/logo.png" alt="Red Bull Racing Logo" height="40" class="me-2">
-          <span class="fw-bold text-warning text-uppercase">Red Bull Racing</span>
-        </a>
-        <p class="text-white-50">Velocidad, innovación y pasión por la Fórmula 1. Vive la emoción con nosotros.</p>
-      </div>
-      <div class="col-md-4 mb-4">
-        <h5 class="text-uppercase text-danger fw-bold">Enlaces</h5>
-        <ul class="list-unstyled">
-          <li><a href="/" class="text-white-50 text-decoration-none">Inicio</a></li>
-          <li><a href="/quienes-somos" class="text-white-50 text-decoration-none">Quiénes Somos</a></li>
-          <li><a href="/acerca" class="text-white-50 text-decoration-none">Acerca de</a></li>
-          <li><a href="/contacto" class="text-white-50 text-decoration-none">Contacto</a></li>
-        </ul>
-      </div>
-      <div class="col-md-4 mb-4">
-        <h5 class="text-uppercase text-danger fw-bold">Síguenos</h5>
-        <div class="d-flex gap-3">
-          <a href="https://www.facebook.com/redbullracing" class="text-white-50 fs-4"><i class="bi bi-facebook"></i></a>
-          <a href="https://twitter.com/redbullracing" class="text-white-50 fs-4"><i class="bi bi-twitter"></i></a>
-          <a href="https://www.instagram.com/redbullracing" class="text-white-50 fs-4"><i class="bi bi-instagram"></i></a>
-          <a href="https://www.youtube.com/redbullracing" class="text-white-50 fs-4"><i class="bi bi-youtube"></i></a>
-        </div>
+    <div class="footer-links">
+      <h5>Enlaces</h5>
+      <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/quienes-somos">Quiénes Somos</a></li>
+        <li><a href="/acerca">Acerca de</a></li>
+        <li><a href="/contacto">Contacto</a></li>
+      </ul>
+    </div>
+
+    <div class="footer-social">
+      <h5>Síguenos</h5>
+      <div class="social-icons">
+        <a href="https://www.facebook.com/redbullracing"><i class="bi bi-facebook"></i></a>
+        <a href="https://twitter.com/redbullracing"><i class="bi bi-twitter"></i></a>
+        <a href="https://www.instagram.com/redbullracing"><i class="bi bi-instagram"></i></a>
+        <a href="https://www.youtube.com/redbullracing"><i class="bi bi-youtube"></i></a>
       </div>
     </div>
-    <hr class="border-top border-danger mt-1">
-    <p class="text-center text-white-50 small mb-0">© 2025 Oracle Red Bull Racing. Todos los derechos reservados.</p>
   </div>
+
+  <hr class="footer-separator">
+  <p class="footer-copy">© 2025 Oracle Red Bull Racing. Todos los derechos reservados.</p>
 </footer>
 <script src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
