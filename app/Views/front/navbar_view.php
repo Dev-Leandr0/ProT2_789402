3f39b09e72df6ef2326582f63236f5070aa5cdb5
Leandro <leandroalegregabriel@outlook.com>
2025-05-21 13:20:10 -0300

    feat(view): agrega íconos de acceso para login y registro en el navbar

--- app/Views/front/navbar_view.php
+++ app/Views/front/navbar_view.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg sticky-top navbar-customizado ">
   <div class="container-fluid animate__animated animate__bounceInLeft">
     <a class="navbar-brand d-flex align-items-center animate__animated animate__pulse animate__delay-1s animate__infinite" href="<?= site_url('principal') ?>">
-      <img src="<?= base_url('assets/img/logo/logo-2.svg') ?>" alt="Red Bull Racing" height="40" class="me-2">
+      <img src="<?= base_url('assets/img/logo/logo-2.svg') ?>" alt="Red Bull Racing" height="30" class="me-2">
     </a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -17,22 +17,17 @@
           <a class="nav-link text-white fw-semibold" href="/quienes-somos">Noticias</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white fw-semibold" href="/acerca">Monoplaza</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle text-white fw-semibold" href="#" role="button" data-bs-toggle="dropdown">
-            Usuarios
-          </a>
-          <ul class="dropdown-menu dropdown-menu-dark">
-            <li><a class="dropdown-item" href="/registrarse">Registrarse</a></li>
-            <li><a class="dropdown-item" href="/login">Login</a></li>
-          </ul>
+          <a class="nav-link text-white fw-semibold" href="<?= site_url('monoplaza') ?>">Monoplaza</a>
         </li>
       </ul>
 
       <form class="d-flex" role="search">
         <input class="form-control me-2 border-0 shadow-sm" type="search" placeholder="Buscar..." aria-label="Buscar">
         <button class="btn btn-danger fw-bold" type="submit">Buscar</button>
+        <button type="button" class="btn btn-outline-light ms-3" data-bs-toggle="modal" data-bs-target="#loginModal">
+          <i class="bi bi-person-circle fs-6"></i>
+        </button>
+
       </form>
     </div>
   </div>
