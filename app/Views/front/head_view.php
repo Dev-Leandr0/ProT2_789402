5ed350911c110b1d90ab3e9a822f3947ed2a849b
Leandro <leandroalegregabriel@outlook.com>
2025-05-21 13:19:40 -0300

    feat(view): agrega links a estilos personalizados y comentarios en head_view

--- app/Views/front/head_view.php
+++ app/Views/front/head_view.php
@@ -5,9 +5,17 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Red Bull Racing</title>
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet"> <!-- iconos bootstrap -->
-  <link rel="stylesheet" href="assets/css/style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" /> <!-- animacion css -->
   <link rel="shortcut icon" href="assets/img/logo/logo-3.png" type="image/x-icon">
+  <!-- Dependencia Bootstrap -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+  <!-- Estilos Personalizados -->
+  <link rel="stylesheet" href="assets/css/style.css"> <!-- Contiene estilos del navbar_view.php / principal.php / footer.php-->
+  <link rel="stylesheet" href="assets/css/style-form.css"> <!-- Contiene estilos de view_login.php y view_registro.php -->
+  <link rel="stylesheet" href="assets/css/style-monoplaza.css"> <!-- Contiene estilos de monoplaza.php -->
+  <!--   <link rel="stylesheet" href="assets/css/style-noticias.css"> -->
+  <!-- Animated css -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+  <!-- Tipografías google -->
+  <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;700&display=swap" rel="stylesheet">
 </head>
