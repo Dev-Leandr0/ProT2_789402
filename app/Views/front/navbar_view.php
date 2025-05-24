430b387e1bb39a886389f0e253c892e77a4d1e35
Leandro <leandroalegregabriel@outlook.com>
2025-05-24 20:03:46 -0300

    [Navbar] Enlaces a 'monoplaza.php' y 'contacto.php' + botón de inicio de sesión con estilos

--- app/Views/front/navbar_view.php
+++ app/Views/front/navbar_view.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg sticky-top navbar-customizado ">
   <div class="container-fluid animate__animated animate__bounceInLeft">
     <a class="navbar-brand d-flex align-items-center animate__animated animate__pulse animate__delay-1s animate__infinite" href="<?= site_url('principal') ?>">
-      <img src="<?= base_url('assets/img/logo/logo-2.svg') ?>" alt="Red Bull Racing" height="30" class="me-2">
+      <img src="<?= base_url('assets/img/icons/logo/logo-2.svg') ?>" alt="Red Bull Racing" height="30" class="me-2">
     </a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -10,21 +10,24 @@
 
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
         <li class="nav-item">
           <a class="nav-link active text-white fw-semibold" href="<?= site_url('principal') ?>">Inicio</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link text-white fw-semibold" href="/quienes-somos">Noticias</a>
-        </li>
+
         <li class="nav-item">
           <a class="nav-link text-white fw-semibold" href="<?= site_url('monoplaza') ?>">Monoplaza</a>
         </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-white fw-semibold" href="<?= site_url('contacto') ?>">Contacto</a>
+
       </ul>
 
       <form class="d-flex" role="search">
         <input class="form-control me-2 border-0 shadow-sm" type="search" placeholder="Buscar..." aria-label="Buscar">
-        <button class="btn btn-danger fw-bold" type="submit">Buscar</button>
-        <button type="button" class="btn btn-outline-light ms-3" data-bs-toggle="modal" data-bs-target="#loginModal">
+        <button class="btn btn-danger fw-bold btn-buscar-icon" type="submit">Buscar</button>
+        <button type="button" class="btn btn-outline-light ms-3 btn-login-icon" data-bs-toggle="modal" data-bs-target="#loginModal">
           <i class="bi bi-person-circle fs-6"></i>
         </button>
 
@@ -32,3 +35,5 @@
     </div>
   </div>
 </nav>
+
+<body>
