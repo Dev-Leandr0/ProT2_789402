575d88112feac3d0f03d912f67af7450e26ef88a
Leandro <leandroalegregabriel@outlook.com>
2025-05-24 20:05:03 -0300

    [Footer] Actualización de íconos de redes sociales a nuevas imágenes desde img/icons/redes-small

--- app/Views/front/footer_view.php
+++ app/Views/front/footer_view.php
@@ -2,7 +2,7 @@
   <div class="footer-container">
     <div class="footer-logo">
       <a href="/" class="footer-marca">
-        <img src="assets/img/logo/logo-4.jpg" alt="Red Bull Racing" height="50">
+        <img src="assets/img/icons/logo/logo-4.jpg" alt="Red Bull Racing" height="50">
         <h5>Red <span class="texto-amarillo">Bull</span> Racing</h5>
       </a>
       <p class="footer-description">
@@ -13,22 +13,31 @@
     <div class="footer-links">
       <h5>Enlaces</h5>
       <ul>
-        <li><a href="/">Inicio</a></li>
-        <li><a href="/quienes-somos">Quiénes Somos</a></li>
-        <li><a href="/acerca">Acerca de</a></li>
-        <li><a href="/contacto">Contacto</a></li>
+        <li><a href="<?= site_url('principal') ?>">Inicio</a></li>
+        <li><a href="<?= site_url('monoplaza') ?>">Monoplaza</a></li>
+        <!-- <li><a href="<?= site_url('pilotos') ?>">Pilotos</a></li> -->
+        <li><a href="<?= site_url('contacto') ?>">Contacto</a></li>
       </ul>
     </div>
 
     <div class="footer-social">
       <h5>Síguenos</h5>
       <div class="social-icons">
-        <a href="https://www.facebook.com/redbullracing"><i class="bi bi-facebook"></i></a>
-        <a href="https://twitter.com/redbullracing"><i class="bi bi-twitter"></i></a>
-        <a href="https://www.instagram.com/redbullracing"><i class="bi bi-instagram"></i></a>
-        <a href="https://www.youtube.com/redbullracing"><i class="bi bi-youtube"></i></a>
+        <a href="https://www.facebook.com/redbullracing">
+          <img src="assets/img/icons/redes-small/facebook.png" alt="Facebook" />
+        </a>
+        <a href="https://twitter.com/redbullracing">
+          <img src="assets/img/icons/redes-small/x.png" alt="Twitter" />
+        </a>
+        <a href="https://www.instagram.com/redbullracing">
+          <img src="assets/img/icons/redes-small/instagram.png" alt="Instagram" />
+        </a>
+        <a href="https://www.youtube.com/redbullracing">
+          <img src="assets/img/icons/redes-small/youtube.png" alt="YouTube" />
+        </a>
       </div>
     </div>
+
   </div>
 
   <hr class="footer-separator">
@@ -37,6 +46,8 @@
 
 <script src="assets/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="<?= base_url('assets/js/contacto.js'); ?>"></script>
 </body>
+
 
 </html>
