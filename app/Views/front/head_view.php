c29ae7a1f96fd8935801a71d87ad49cab56e3307
Leandro <leandroalegregabriel@outlook.com>
2025-05-26 12:17:25 -0300

    [Views] Comentarios agregados y eliminado link a style-monoplaza.css en head_view.php

--- app/Views/front/head_view.php
+++ app/Views/front/head_view.php
@@ -6,16 +6,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Red Bull Racing</title>
   <link rel="shortcut icon" href="assets/img/icons/logo/logo-3.png" type="image/x-icon">
+
   <!-- Dependencia Bootstrap -->
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+
   <!-- Estilos Personalizados -->
-  <link rel="stylesheet" href="assets/css/style.css"> <!-- Contiene estilos del navbar_view.php / principal.php / footer.php-->
+  <link rel="stylesheet" href="assets/css/style.css"> <!-- Contiene estilos del navbar_view.php / principal.php / monoplaza.php /contacto.php / footer.php-->
   <link rel="stylesheet" href="assets/css/style-form.css"> <!-- Contiene estilos de view_login.php y view_registro.php -->
-  <link rel="stylesheet" href="assets/css/style-monoplaza.css"> <!-- Contiene estilos de monoplaza.php -->
-  <!--   <link rel="stylesheet" href="assets/css/style-noticias.css"> -->
+
   <!-- Animated css -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
   <!-- Tipografías google -->
   <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;700&display=swap" rel="stylesheet">
+
 </head>
