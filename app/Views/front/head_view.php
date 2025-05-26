15741d39cf7abebae50dcf445e2edc8b61ee98b3
Leandro <leandroalegregabriel@outlook.com>
2025-05-26 20:47:21 -0300

    [Head] Reemplazados links por href con base_url para mejor manejo de rutas

--- app/Views/front/head_view.php
+++ app/Views/front/head_view.php
@@ -5,19 +5,18 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Red Bull Racing</title>
-  <link rel="shortcut icon" href="assets/img/icons/logo/logo-3.png" type="image/x-icon">
+  <link rel="shortcut icon" href="<?= base_url('assets/img/icons/logo/logo-3.png') ?>" type="image/x-icon">
 
   <!-- Dependencia Bootstrap -->
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
   <!-- Estilos Personalizados -->
-  <link rel="stylesheet" href="assets/css/style.css"> <!-- Contiene estilos del navbar_view.php / principal.php / monoplaza.php /contacto.php / footer.php / form_view.php -->
+  <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>"> <!-- Contiene estilos del navbar_view.php / principal.php / monoplaza.php / contacto.php / footer.php / form_view.php -->
 
   <!-- Animated css -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
   <!-- Tipografías google -->
   <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;700&display=swap" rel="stylesheet">
-
 </head>
