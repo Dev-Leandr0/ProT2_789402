a553b1e428b8528ac7016493af0d637fd89714a4
Leandro <leandroalegregabriel@outlook.com>
2025-05-26 17:52:39 -0300

    [Head] Eliminado enlace a style-form.css tras migración de estilos a style.css

--- app/Views/front/head_view.php
+++ app/Views/front/head_view.php
@@ -12,8 +12,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
   <!-- Estilos Personalizados -->
-  <link rel="stylesheet" href="assets/css/style.css"> <!-- Contiene estilos del navbar_view.php / principal.php / monoplaza.php /contacto.php / footer.php-->
-  <link rel="stylesheet" href="assets/css/style-form.css"> <!-- Contiene estilos de view_login.php y view_registro.php -->
+  <link rel="stylesheet" href="assets/css/style.css"> <!-- Contiene estilos del navbar_view.php / principal.php / monoplaza.php /contacto.php / footer.php / form_view.php -->
 
   <!-- Animated css -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
